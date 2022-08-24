3b91829b1dc0abe1bbedcd58a9debb17bc7185f7
Aybars Ünlü <aybars-unlu@hotmail.com>
2022-08-24 22:41:05 +0300

    Patika.dev Workspace
    Insertion Sort Workspace for Patika.dev

---
+++ patika/Insertion Sort Projesi.py
@@ -0,0 +1,20 @@
+"1. Yukarıda verilen dizinin sort türüne göre aşamalarını yazınız"
+
+- 6 ile 22 yer değişir [6,27,16,2,18,22]
+- 2 ile 27 yer değişir [6,2,16,27,18,22]
+- 16 sabir kalır       [6,2,16,27,18,22]
+- 18 ile 27 yer değişir[6,2,16,18,27,22]
+- 22 ile 27 yer değişir[6,2,16,18,22,27]
+
+"2.Big-O gösterimini yazınız"
+Big-O = n^2
+
+"3. Time Complexity: Average case: Aradığımız sayının ortada olması,Worst case: Aradığımız sayının sonda olması, Best case: Aradığımız sayının dizinin en başında olması.
+"Dizi sıralandıktan sonra 18 sayısı hangi case kapsamına girer? Yazınız."
+Average Case
+
+"4.[7,3,5,8,2,9,4,15,6] dizisinin Insertion Sort'a göre ilk 4 adımını yazınız."
+2 ile 7 yer değişir [2,3,5,8,7,9,4,15,6]
+3 sabit kalır       [2,3,5,8,7,9,4,15,6]
+4 ile 5 yer değişir [2,3,4,8,7,9,5,15,6]
+5 ile 8 yer değişir [2,3,4,5,7,9,8,15,6]
